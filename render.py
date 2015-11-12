9eb29f2eaf215ac14a91775ab8127e64dab1c3d0
Bas Pennings <baspster@gmail.com>
2015-11-12 20:24:26 +0100

    Trying to organize my ideas

--- render.py
+++ render.py
@@ -1,13 +1,11 @@
+from collections import namedtuple
 import re
 
 #
 # ident = [a-zA-Z_][a-zA-Z0-9_]*
 # style = <renderer>(obj[.attr])
-
-# We might think we have two kinds of renderers but we do not.
-# They are exactly the same if we consider one a limited version
-# of the other.
 #
+
 # Globally there are a few main things a renderer should do:
 #
 # 1. Escape if necessary
@@ -19,5 +17,19 @@ import re
 # Could do a rendering-chain where we have a next renderer 
 # instead of just an array of render functions.
 
-		
-	 
+#
+# * We have push tags and pop tags.
+# * A push tag pushes a style onto the rendering stack
+# * A pop tag pops a style from the rendering stack
+# * A renderer is responsible for looking up a particular style
+# * Of course it can delegate this task how it desires
+# * A client should be able to clearle control and specify the rendering pipeline
+# * There's a single reserved style _default
+# * Every renderer should support this style
+# * There's probably also a reserved renderer %(<obj>[.<attr>])
+
+#
+# Instruction format: <style(obj[.attr] /> or it can also be a <style(obj[.attr]> to </antyhing_goes_here>
+# The first one will just execute the render function, replace the rendering tag and pop the stack.
+# The second one will execute the render function and will pop the stack when it encounters the end tag.
+
